c5108aa67e0fe7927a81cd950ef2069a1e872064
Fifty <fifty@patch.com>
2013-07-07 20:00:02 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orangedesk::Application.config.secret_key_base = '2a0126039b5cb5f495c1c75d21981f6d18dffe6c481378f9626111685703b648602e2fd1722ca6b84c1c9d06614804762f72c453d3555cf52db54dbf725cf63f'
